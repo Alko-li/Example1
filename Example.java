a85e5012ee0c06ac7c1d86436b96e84ee8c60663
Albert Yiakiam Ko <Nimbro@lawn-128-61-57-36.lawn.gatech.edu>
2019-09-09 19:09:31 -0400

    Added a closing bye added a closing bye, replaced wow with hello world

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 public class Example {
     public static void main(String[] args) {
-        System.out.println("WOW");
+        System.out.println("Hello World!");
+        System.out.println("Bye!");
     }
 }
